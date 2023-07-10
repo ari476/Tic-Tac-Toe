297e4993d9737c24d1bd86842bcdafff1aafd0a2
ari refael <AriRe@edu.aac.ac.il>
2023-07-10 16:22:38 +0300

    change comment

--- ai_player.py
+++ ai_player.py
@@ -6,10 +6,10 @@ from typing import List
 def make_decision(board: List[List[Player]], ai_player: Player) -> tuple:
     """
     Args:
-        board (List[List[Player]]): board of the game
+        board (List[List[Player]]): current state of board in the game
         ai_player (Player): X or O enum 
 
     Returns:
-        tuple: the current move of ai
+        tuple: cell position for placing the next symbol on the game board
     """
     pass
