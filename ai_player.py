abd6a45d2d3fb3f1250c81c8b3f81e456190c628
ari refael <AriRe@edu.aac.ac.il>
2023-07-10 14:48:05 +0300

    separate logic and gui

---
+++ ai_player.py
@@ -0,0 +1,15 @@
+from game_logic import Player
+from typing import List
+
+
+
+def make_decision(board: List[List[Player]], ai_player: Player) -> tuple:
+    """
+    Args:
+        board (List[List[Player]]): board of the game
+        ai_player (Player): X or O enum 
+
+    Returns:
+        tuple: the current move of ai
+    """
+    pass
---
+++ game_logic.py
@@ -0,0 +1,54 @@
+from enum import Enum
+
+
+class Player(Enum):
+    X = 'X'
+    O = 'O'
+
+
+class TicTacToe:
+    def __init__(self):
+        self.current_player = Player.X
+        self.board = [[None for _ in range(3)] for _ in range(3)]
+
+    def is_cell_empty(self, cell_pos):
+        return self.board[cell_pos[0]][cell_pos[1]] is None
+
+    def is_winners(self):
+        """
+        check if self.current_player win: 
+        """
+
+        # Check rows
+        for row in self.board:
+            if row.count(self.current_player) == 3:
+                return True
+
+        # Check columns
+        for col in range(3):
+            if all(self.board[row][col] == self.current_player for row in range(3)):
+                return True
+
+        # Check diagonals
+        if self.board[0][0] == self.board[1][1] == self.board[2][2] == self.current_player or \
+           self.board[0][2] == self.board[1][1] == self.board[2][0] == self.current_player:
+            return True
+
+        return False
+
+    def is_cells_occupied(self):
+        return all(self.board[row][col] is not None for row in range(3) for col in range(3))
+
+    def is_game_over(self):
+        """
+        if one from the player won return him.
+        if the result game is tie return 'Tie'.
+        in any other case return False.
+        """
+        if self.is_winners():
+            return self.current_player
+
+        if self.is_cells_occupied():
+            return 'Tie'
+
+        return False
