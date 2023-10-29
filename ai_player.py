93836ae33a5671d784cb2d4f90a6646d9e101923
a0526604945@gmail.com <hen>
2023-10-29 18:47:47 +0200

    make the code look better

--- ai_player.py
+++ ai_player.py
@@ -11,7 +11,6 @@ def make_decision(board: List[List[Player]], ai_player: Player) -> tuple:
     for row in range(3):
         result =[board[row][col] for col in range(3) if board[row][col] == ai_player]
         if len(result) ==2:
-            print("work")
             if board[row][0] == None:
                 return (row,0)
             elif board[row][1] == None:
@@ -23,7 +22,6 @@ def make_decision(board: List[List[Player]], ai_player: Player) -> tuple:
     for col in range(3):
         result = [board[row][col] for row in range(3) if board[row][col] == ai_player]
         if len(result) == 2:
-            print("work")
             if board[0][col] == None:
                 return (0,col)
             elif board[1][col] == None:
@@ -32,22 +30,16 @@ def make_decision(board: List[List[Player]], ai_player: Player) -> tuple:
                 return (2,col)
             
     if board[0][0] == board[1][1] == ai_player and board[2][2] == None:
-        print("work")
         return (2,2)
     if board[0][0] == board[2][2] == ai_player and board[1][1] == None:
-        print("work")
         return (1,1)
     if board[2][2] == board[1][1] == ai_player and board[0][0] == None:
-        print("work")
         return (0,0)
     if board[0][2] == board[1][1] == ai_player and board[2][0] == None:
-        print("work")
         return (2,0)
     if board[0][2] == board[2][0] == ai_player and board[1][1] == None:
-        print("work")
         return (1,1)
     if board[2][0] == board[1][1] == ai_player and board[0][2] == None:
-        print("work")
         return (0,2)
     
     ############################################################
@@ -56,19 +48,16 @@ def make_decision(board: List[List[Player]], ai_player: Player) -> tuple:
     for row in range(3):
         result =[board[row][col] for col in range(3) if board[row][col] != ai_player and board[row][col] is not None]
         if len(result) ==2:
-            print("work")
             if board[row][0] == None:
                 return (row,0)
             elif board[row][1] == None:
                 return (row,1)
             elif board[row][2] == None:
                 return (row,2)
-
-                   
+        
     for col in range(3):
         result = [board[row][col] for row in range(3) if board[row][col] != ai_player and board[row][col] is not None]
         if len(result) == 2:
-            print("work")
             if board[0][col] == None:
                 return (0,col)
             elif board[1][col] == None:
@@ -77,22 +66,16 @@ def make_decision(board: List[List[Player]], ai_player: Player) -> tuple:
                 return (2,col)
             
     if board[0][0] == board[1][1] and board[0][0] != ai_player and board[2][2] is None and board[0][0] is not None:
-        print("work")
         return (2,2)
     if board[0][0] == board[2][2] and board[0][0]!= ai_player and board[1][1] is None and board[0][0] is not None:
-        print("work")
         return (1,1)
     if board[2][2] == board[1][1] and board[2][2]!= ai_player and board[0][0] is None and board[2][2] is not None:
-        print("work")
         return (0,0)
     if board[0][2] == board[1][1] and board[0][2]!= ai_player and board[2][0] is None and board[0][2] is not None:
-        print("work")
         return (2,0)
     if board[0][2] == board[2][0] and board[0][2]!= ai_player and board[1][1] is None and board[0][2] is not None:
-        print("work")
         return (1,1)
     if board[2][0] == board[1][1] and board[2][0]!= ai_player and board[0][2] is None and board[2][0] is not None:
-        print("work")
         return (0,2)
     
     ############################################################
