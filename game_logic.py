d6085f445a1da0d9bb9e5a48509e54b11696bdc3
yoda-gell <hvuskv6@gmail.com>
2023-10-30 11:31:05 +0200

    with no change

--- game_logic.py
+++ game_logic.py
@@ -5,13 +5,14 @@ class Player(Enum):
     X = 'X'
     O = 'O'
 
+
 class TicTacToe:
-    def __init__(self, current_player = None, board = None):     
-        self.current_player = current_player or Player.X 
-        self.board = copy.deepcopy(board) or [[None for _ in range(3)] for _ in range(3)] 
+    def __init__(self, current_player = None, board = None):
+        self.current_player = current_player or Player.X
+        self.board = copy.deepcopy(board) or [[None for _ in range(3)] for _ in range(3)]
 
     def is_cell_empty(self, cell_pos):
-        return self.board[cell_pos[0]][cell_pos[1]] is None    
+        return self.board[cell_pos[0]][cell_pos[1]] is None
 
     def is_winners(self):
         """
@@ -35,7 +36,7 @@ class TicTacToe:
 
         return False
 
-    def is_cells_occupied(self):  
+    def is_cells_occupied(self):
         return all(self.board[row][col] is not None for row in range(3) for col in range(3))
 
     def is_game_over(self):
