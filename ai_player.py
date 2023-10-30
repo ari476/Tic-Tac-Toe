c4c0fdff3be2ad32130f1421918b1c671a374381
a0526604945@gmail.com <hen>
2023-10-30 10:07:57 +0200

    fix bug

--- ai_player.py
+++ ai_player.py
@@ -86,21 +86,55 @@ def make_decision(board: List[List[Player]], ai_player: Player) -> tuple:
         if board[cell[0]][cell[1]] is not None and board[cell[0]][cell[1]] != ai_player:
             if board[1][1] is None:
                 return (1,1)
-            else:
-                if cell[0] == 0 and cell[1] == 1:
-                    if board[1][0] != ai_player and board[1][0] is not None:
-                        if board[0][0] is None:
-                            return (0,0)
-                    elif board[1][2] != ai_player and board[1][2] is not None:
-                        if board[0][2] is None:
-                            return (0,2)
-                elif cell[0] == 2 and cell[1] == 1:
-                    if board[1][0] != ai_player and board[1][0] is not None:
-                        if board[2][0] is None:
-                            return (2,0)
-                    elif board[1][2] != ai_player and board[1][2] is not None:
-                        if board[2][2] is None:
-                            return (2,2)
+
+    if board[0][1] != ai_player and board[0][1] is not None:
+        if board[1][0] != ai_player and board[1][0] is not None:
+            if board[0][0] is None:
+                return (0,0)
+        elif board[2][0] != ai_player and board[2][0] is not None:
+            if board[0][0] is None:
+                return (0,0)
+        elif board[1][2] != ai_player and board[1][2] is not None:
+            if board[0][2] is None:
+                return (0,2)
+        elif board[2][2] != ai_player and board[2][2] is not None:
+            if board[0][2] is None:
+                return (0,2)
+    elif board[2][1] != ai_player and board[2][1] is not None:
+        if board[1][0] != ai_player and board[1][0] is not None:
+            if board[2][0] is None:
+                return (2,0)
+        elif board[0][0] != ai_player and board[0][0] is not None:
+            if board[2][0] is None:
+                return (2,0)
+        elif board[1][2] != ai_player and board[1][2] is not None:
+            if board[2][2] is None:
+                return (2,2)
+        elif board[0][2] != ai_player and board[0][2] is not None:
+            if board[2][2] is None:
+                return (2,2)
+    elif board[1][0] != ai_player and board[1][0] is not None: 
+        if board[0][2] != ai_player and board[0][2] is not None:
+            if board[0][0] is None:
+                return (0,0)  
+        elif board[2][2] != ai_player and board[2][2] is not None:
+            if board[2][0] is None:
+                return (2,0) 
+    elif board[1][2] != ai_player and board[1][2] is not None:
+        if board[0][0] != ai_player and board[0][0] is not None:
+            if board[0][2] is None:
+                return (0,2) 
+        elif board[2][0] != ai_player and board[2][0] is not None:
+            if board[2][2] is None:
+                return (2,2)   
+
+    for corner in corners:
+        if board[corner[0]][corner[1]] != ai_player and board[corner[0]][corner[1]] != None:
+            plus = [(0,1),(1,0),(1,2),(2,1)]
+            for p in plus:
+                if board[p[0]][p[1]] is None:
+                    return p 
+     
     for corner in corners:
         if board[corner[0]][corner[1]] is None:
             return corner
@@ -108,4 +142,4 @@ def make_decision(board: List[List[Player]], ai_player: Player) -> tuple:
     plus = [(0,1),(1,0),(1,2),(2,1)]
     for p in plus:
         if board[p[0]][p[1]] is None:
-            return p
+            return p  
