d9b0efb045104f77c984de5fd8154432eccb1001
Shahaf Gressel <shahafgressel@Shahafs-MacBook-Air.local>
2023-11-01 18:22:37 +0200

    new code, before run

--- ai_player.py
+++ ai_player.py
@@ -3,31 +3,24 @@ from typing import List
 from game_logic import TicTacToe
 import random
 
-# This function returns true if there are moves  
-# remaining on the board. It returns false if  
-# there are no moves left to play.  
-def is_moves_left(board) :  
+def is_moves_left(board) :  #checks if there are moves left to do
 
     for row in range(3) : 
         for col in range(3) : 
             if (board[row][col] == None) : 
                 return True 
     return False
-
-# This is the evaluation function as discussed  
-# in the previous article ( http://goo.gl/sJgv68 )  
-def evaluate(board) :  
+ 
+def evaluate_move(board, player, opponent) :  
     
-    # Checking for Rows for X or O victory.  
-    for row in range(3) :      
+    for row in range(3) :     #checks victory in row (x or o)
         if (board[row][0] == board[row][1] and board[row][1] == board[row][2]) :         
             if (board[row][0] == player) : 
                 return 10
             elif (board[row][0] == opponent) : 
                 return -10
 
-    # Checking for Columns for X or O victory.  
-    for col in range(3) : 
+    for col in range(3) : #checks victory in column (x or o)
     
         if (board[0][col] == board[1][col] and board[1][col] == board[2][col]) : 
         
@@ -36,8 +29,7 @@ def evaluate(board) :
             elif (board[0][col] == opponent) : 
                 return -10
 
-    # Checking for Diagonals for X or O victory.  
-    if (board[0][0] == board[1][1] and board[1][1] == board[2][2]) : 
+    if (board[0][0] == board[1][1] and board[1][1] == board[2][2]) : #checks victory in diagonal (x or o)
     
         if (board[0][0] == player) : 
             return 10
@@ -51,78 +43,46 @@ def evaluate(board) :
         elif (board[0][2] == opponent) : 
             return -10
 
-    # Else if none of them have won then return 0  
-    return 0
 
-# This is the minimax function. It considers all  
-# the possible ways the game can go and returns  
-# the value of the board  
-def minimax(board, depth, isMax) :  
-    score = evaluate(board) 
+    return 0 # returns 0 if there are no wins
 
-    # If Maximizer has won the game return his/her  
-    # evaluated score  
+def minimax(board, depth, is_max, player, opponent) :  #minimax function (checks all ways the game could go) returns 10 for win for ai, -10 for lose and 0 for tie
+    score = evaluate_move(board, player, opponent) 
+
     if (score == 10) :  
         return score 
 
-    # If Minimizer has won the game return his/her  
-    # evaluated score  
-    if (score == -10) : 
+    elif (score == -10) : 
         return score 
 
-    # If there are no more moves and no winner then  
-    # it is a tie  
-    if (is_moves_left(board) == False) : 
+    elif (is_moves_left(board) == False) : 
         return 0
 
-    # If this maximizer's move  
-    if (isMax) :      
+    if (is_max) :      #when it is the ai move
         best = -1000 
 
-        # Traverse all cells  
-        for i in range(3) :          
-            for j in range(3) : 
+        for row in range(3) :          
+            for col in range(3) : 
             
-                # Check if cell is empty  
-                if (board[i][j]=='_') : 
-                
-                    # Make the move  
-                    board[i][j] = player  
-
-                    # Call minimax recursively and choose  
-                    # the maximum value  
-                    best = max( best, minimax(board, 
-                                            depth + 1, 
-                                            not isMax) ) 
-
-                    # Undo the move  
-                    board[i][j] = '_'
+                if (board[row][col] == None) : 
+                    board[row][col] = player  #tries the move
+                    best = max( best, minimax(board, depth + 1, not is_max, player, opponent) ) #checks if this move is the best
+                    board[row][col] = None  #delete the move
         return best 
 
-    # If this minimizer's move  
-    else : 
+    else :        #when it is the enemy move
         best = 1000 
 
-        # Traverse all cells  
-        for i in range(3) :          
-            for j in range(3) : 
+        for row in range(3) :          
+            for col in range(3) : 
             
-                # Check if cell is empty  
-                if (board[i][j] == '_') : 
-                
-                    # Make the move  
-                    board[i][j] = opponent  
-
-                    # Call minimax recursively and choose  
-                    # the minimum value  
-                    best = min(best, minimax(board, depth + 1, not isMax)) 
-
-                    # Undo the move  
-                    board[i][j] = '_'
+                if (board[row][col] == None) : 
+                    board[row][col] = opponent  
+                    best = min(best, minimax(board, depth + 1, not is_max, player, opponent)) 
+                    board[row][col] = None
         return best 
 
-# This will return the best possible move for the player  
-def make_decision(board: List[List[Player]], ai_player: Player) -> tuple:
+def make_decision(board: List[List[Player]], ai_player: Player) -> tuple: #returns best move for ai
     """
     Args:
         board (List[List[Player]]): current state of board in the game
@@ -138,32 +98,20 @@ def make_decision(board: List[List[Player]], ai_player: Player) -> tuple:
     else:
         opponent = Player.X
         player = Player.O
-    bestVal = -1000 
-    bestMove = (-1, -1)  
 
-    # Traverse all cells, evaluate minimax function for  
-    # all empty cells. And return the cell with optimal  
-    # value.  
+    best_val = -1000 
+    best_move = (-1, -1)  
+
     for row in range(3) :      
         for col in range(3) : 
-        
-            # Check if cell is empty  
+
             if (board[row][col] == None) :  
-            
-                # Make the move  
                 board[row][col] = player 
-
-                # compute evaluation function for this  
-                # move.  
-                moveVal = minimax(board, 0, False)  
-
-                # Undo the move  
+                move_val = minimax(board, 0, False, player, opponent)  
                 board[row][col] = None
 
-                # If the value of the current move is  
-                # more than the best value, then update  
-                # best/  
-                if (moveVal > bestVal) :                 
-                    bestMove = (row, col) 
-                    bestVal = moveVal 
-    return bestMove 
+                if (move_val > best_val) :                 
+                    best_move = (row, col) 
+                    best_val = move_val 
+
+    return best_move 
